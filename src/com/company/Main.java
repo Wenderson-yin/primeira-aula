1490b0890ffd22b5ca4ee43d1caecca044720b4d
Wenderson rei delas <wenderson.leandro194@gmail.com>
2022-08-02 21:13:19 -0300

    segunda função

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,14 @@ package com.company;
 
 public class Main {
 
+
     public static void main(String[] args) {
         funcaoSoma(2, 2);
+        funcaoSubtair(2,5);
     }
-
+    public static void funcaoSubtair(double x, double y){
+        double z = x + y;
+    }
     public static void funcaoSoma(int x, int y){
         int z = x+y;
         System.out.println("A soma de " + x + " mais " + y + " é igual a: " + z);
