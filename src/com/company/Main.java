0f17547b4df0e46a02ac6264d59bff36e56d676f
Wenderson rei delas <wenderson.leandro194@gmail.com>
2022-08-02 21:44:59 -0300

    metodo edição de strings

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,25 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 
     public static void main(String[] args) {
         funcaoSoma(2, 2);
         funcaoSubtair(2,5);
+        List<String> x = nomesDeAlunos("wenderson", "lucas");
+        for (String s: x){
+            System.out.println(s);
+        }
     }
+    public static List<String> nomesDeAlunos(String s, String str){
+        List<String> listString = new ArrayList<>();
+            listString.add(s);
+            listString.add(str);
+            return listString;
+        }
     public static void funcaoSubtair(double x, double y){
         double z = x + y;
     }
