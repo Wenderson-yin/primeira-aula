2dd032881e8a738bde2ec0975d62f888ab64882b
Wenderson rei delas <wenderson.leandro194@gmail.com>
2022-08-02 22:07:24 -0300

    mais codigo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,22 +2,34 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        funcaoSoma(2, 2);
-        funcaoSubtair(2,5);
-        List<String> x = nomesDeAlunos("wenderson", "lucas");
-        for (String s: x){
-            System.out.println(s);
-        }
+//        funcaoSoma(2, 2);
+//        funcaoSubtair(2,5);
+//        List<String> x = nomesDeAlunos("wenderson", "lucas", "paulo");
+//        for (String s: x){
+//            System.out.println(s);
+//        }
+//        System.out.println(x.get(1));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual é seu nome?");
+        String s = scanner.nextLine();
+        System.out.println("Que ano você nasceu?");
+        int ano = Integer.parseInt(scanner.nextLine());
+        int idade = 2022-ano;
+        System.out.println("Nome:" + s );
+        System.out.println("Idade: " + idade);
+
     }
-    public static List<String> nomesDeAlunos(String s, String str){
+    public static List<String> nomesDeAlunos(String s, String str, String a){
         List<String> listString = new ArrayList<>();
             listString.add(s);
             listString.add(str);
+            listString.add(a);
             return listString;
         }
     public static void funcaoSubtair(double x, double y){
